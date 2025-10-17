def82068ab91305b7a441d399247a37cf8e6dc11
Ashfarell <ashfarell@gmail.com>
2025-10-16 21:12:41 -0300

    M11 inclusão produtos

--- cypress/e2e/3-M11/produtos.cy.js
+++ cypress/e2e/3-M11/produtos.cy.js
@@ -1,1 +1,17 @@
 /// <reference types="cypress" />
+
+describe('Funcionalidade: Produto', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        //.first()  // busca o primeiro na fila
+        //.last()   // busca o último na fila
+        //.eq(3)   // busca 3º na fila
+        .contains('Abominable Hoodie')  // busca o nome exato
+        .click()
+    })
+
+});
